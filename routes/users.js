da4e4afe5cc745f40f0d90f1571bc70fe8570c89
David Hillman <dhillman1989@gmail.com>
2021-03-16 17:14:43 +0000

    added basic error feedback to signin form

--- routes/users.js
+++ routes/users.js
@@ -16,26 +16,33 @@ router.get("/signup", (req, res, next) => {
 router.post("/signup", async (req, res, next) => {
   const { username, password, confirmPassword } = req.body;
   const user = await User.findOne({ username });
+  const errors = [];
   if (!username || !password || !confirmPassword) {
-    return res.send("please provide all details");
-  } else if (password !== confirmPassword) {
-    return res.send("passwords must match");
-  } else if (user) {
-    return res.send("User already exits");
+    errors.push("All fields are required");
   }
-  const cleanUsername = username.trim().toLowerCase();
-  bcrypt.hash(password, 8, async function (err, hash) {
-    const user = await new User({
-      username: cleanUsername,
-      password: hash,
-    }).save();
-    req.login(user, function (err) {
-      if (err) {
-        return next(err);
-      }
-      return res.redirect("/");
+  if (password !== confirmPassword) {
+    errors.push("Passwords must match");
+  }
+  if (user) {
+    errors.push("A user with this username already exists");
+  }
+  if (errors.length) {
+    res.render("signupform", { errors });
+  } else {
+    const cleanUsername = username.trim().toLowerCase();
+    bcrypt.hash(password, 8, async function (err, hash) {
+      const user = await new User({
+        username: cleanUsername,
+        password: hash,
+      }).save();
+      req.login(user, function (err) {
+        if (err) {
+          return next(err);
+        }
+        return res.redirect("/");
+      });
     });
-  });
+  }
 });
 
 router.get("/login", (req, res, next) => {
